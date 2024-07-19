a9be8bb7789d7923a7020e7cc9725cee605fd385
caesiumy <dbs2636@gmail.com>
2024-07-19 19:40:12 +0900

    feat: todoClient 추가

---
+++ src/api/client.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const todoClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*", // CORS
+  },
+});
+
+todoClient.interceptors.request.use((config) => {
+  console.log("config :>> ", config);
+});
+
+todoClient.interceptors.response.use((response) => {
+  console.log("response :>> ", response);
+});
---
+++ src/api/todos.js
@@ -0,0 +1,25 @@
+import { todoClient } from "./client";
+
+export const getTodos = async () => {
+  const response = await todoClient.get("/todos");
+
+  return response.data;
+};
+
+export const createTodo = async (todo) => {
+  const response = await todoClient.post("/todos", todo);
+
+  return response.data;
+};
+
+export const updateTodo = async (todo) => {
+  const response = await todoClient.put(`/todos/${todo.id}`, todo);
+
+  return response.data;
+};
+
+export const deleteTodo = async (id) => {
+  const response = await todoClient.delete(`/todos/${id}`);
+
+  return response.data;
+};
